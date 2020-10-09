08a46b457ca05d4b1459896f756c8ce58fa9d8c1
EmmanuelN-L <46698046+EmmanuelN-L@users.noreply.github.com>
2020-10-09 02:46:08 -0400

    Alot has happened to this server code
    I did not finish lab 2 so I put in the posted code. Then did the requirements for assignment 2 which you can specifically see in the connection loop when I receive positionData from the client . In the Game loop I sent the position data to all players. Also updated the packets per second to 30!

--- server.py
+++ server.py
@@ -5,94 +5,205 @@ from _thread import *
 import threading
 from datetime import datetime
 import json
+import ast
 
+########################
+""" GLOBALS """
+########################
+# creating a lock for shared threads https://realpython.com/intro-to-python-threading/#working-with-many-threads
 clients_lock = threading.Lock()
+# num of connected clients
 connected = 0
-clients = {} # Creating an array that can have individual dictionaries
+# a dictionary that will store our clients indexed by their (IP, PORT)
+clients = {}
+
+########################
+""" METHODS """
+########################
+
 
 def connectionLoop(sock):
-   while True:
-      data, addr = sock.recvfrom(1024)
-      data = str(data)
-      if addr in clients:
-         if 'heartbeat' in data:
-            clients[addr]['lastBeat'] = datetime.now()
-      else:
-         if 'connect' in data:
-            clients[addr] = {}
-            clients[addr]['lastBeat'] = datetime.now()
-            clients[addr]['color'] = 0
-      # Sending a list of current players to new player 
+    """
+    Takes care of:
+      1. Updating the heartbeat of an existing client
+      2. Connecting a new client 
+    """
+    while True:
+        # Listen to the next message
+        data, addr = sock.recvfrom(1024)
+        data = str(data, 'utf-8') #Took me too long to figure this out 
+        print("Got this: " + data)
 
-            playerList = {"List": []}
-            for c in clients:
-               templist = {"OtherPlayers":{"IPandPort":str(c)}}
-               playerList["List"].append(templist)
+        # if addr (i.e IP,PORT) exists in clients dictionary
+        if addr in clients:
+            # update the heartbeat value if data dictionary has a key called 'heartbeat'
+            if 'heartbeat' in data:
+                clients[addr]['lastBeat'] = datetime.now()
+                #If localPos is in the data it go to this code
+                #Now the positions are able to update constantly
+            if 'localPos' in data: 
+                print("Got this: " + data)
+                #Converting the json string to a dictionary
+                positionData = json.loads(data) 
 
-               
-            plist = json.dumps(playerList)
-            sock.sendto(bytes(plist,'utf8'), (addr[0],addr[1]))
-            message = {"cmd": 0,"player":{"id":str(addr)}}
-            m = json.dumps(message)
-         
-            #sock.sendto(bytes(m,'utf8'), (addr[0],addr[1]))
-            for c in clients:
-               sock.sendto(bytes(m,'utf8'), (c[0],c[1]))
+                clients[addr]['playerPosition'] = {
+                    "x": positionData['localPos']['x'],
+                    "y": positionData['localPos']['y'],
+                    "z": positionData['localPos']['z']
+                }
+        else:
+            # if there is a key called 'connect' in data dictionary
+            if 'connect' in data:
+                # add a new object to the client dictionary
+                clients[addr] = {}
+                # update the last beat of the client object
+                clients[addr]['lastBeat'] = datetime.now()
+                # add a field called color
+                clients[addr]['color'] = 0
+                clients[addr]['playerPosition'] = 0
+                # create a message object with a command value and an array of player objects
+                message = {"cmd": 0, "players": []}  # {"id":addr}}
+
+                # create a new object
+                p = {}
+                # add a field called 'id' that is the string version of (IP, PORT)
+                p['id'] = str(addr)
+                # create a field called color
+                p['color'] = 0
+                p['playerPosition'] = 0
+                # add the object to the player array
+                message['players'].append(p)
+
+                # create a new gamestate object similar to message
+                GameState = {"cmd": 4, "players": []}
+                # for every key of clients
+                for c in clients:
+                    # if the key is the same as the connected player
+                    if (c == addr):
+                        # change command to 3
+                        message['cmd'] = 3
+                    else:
+                        message['cmd'] = 0
+
+                    # create a JSON string.
+                    # google what the separator function does. Why do we use it here? Its not always needed.
+                    m = json.dumps(message, separators=(",", ":"))
+
+                    # create a new player object
+                    player = {}
+                    # set the id to the current key
+                    player['id'] = str(c)
+                    # set the color to the key's properties
+                    player['color'] = clients[c]['color']
+                    player['playerPosition'] = clients[c]['playerPosition']
+                    # add it to the game state
+                    GameState['players'].append(player)
+                    # send the message object containg the new connected client to the previously connected clients
+                    sock.sendto(bytes(m, 'utf8'), (c[0], c[1]))
+
+                # send the game state to the new client
+                m = json.dumps(GameState)
+                sock.sendto(bytes(m, 'utf8'), addr)
+
 
 def cleanClients(sock):
-   while True:
-      for c in list(clients.keys()):
-         if (datetime.now() - clients[c]['lastBeat']).total_seconds() > 5:
-            print('Dropped Client: ', c)  
+    """
+      Takes care of:
+         1. Checking if a client should be dropped
+         2. Letting the clients know of a drop
+    """
+    while True:
+        # create an array
+        droppedClients = []
+        # for every client in keys
+        # How is this different from what we did in line 67? Try and find out.
+        for c in list(clients.keys()):
+            # Check if its been longer than 5 seconds since the last heartbeat
+            if (datetime.now() - clients[c]['lastBeat']).total_seconds() > 5:
+                print('Dropped Client: ', c)
+                # for thread safety, gain the lock
+                clients_lock.acquire()
+                # delete the client identified by c
+                del clients[c]
+                # releast the lock we have
+                clients_lock.release()
+                # add the dropped client key to the array of dropped clients
+                droppedClients.append(str(c))
 
-            droppedList = {"Yeeted from the server":{"ID":str(c)}}
-            dlist = json.dumps(droppedList)
-            for d in clients: 
-             sock.sendto(bytes(dlist, 'utf8'), (d[0],d[1]))
+        # Send a JSON object with list of dropped clients to all connected clients
+        message = {"cmd": 2, "droppedPlayers": droppedClients}
+        m = json.dumps(message, separators=(",", ":"))
 
-              
+        if (len(droppedClients) > 0):
+            for c in clients:
+                sock.sendto(bytes(m, 'utf8'), (c[0], c[1]))
 
-            clients_lock.acquire()
-            del clients[c]
-            clients_lock.release()
+        time.sleep(1)
 
-      time.sleep(1)
-
-# addr = IP and PORT, 2 VALUES
-      # clients[addr 1 = 0] = {"lastBeat: 10:50pm, "color: {"R": random.random(), "G": random.random(), "B": random.random()}"}
-
-      # clients[addr 2 = 1] = {"lastBeat: 10:45pm, "color: 2"}
-
-      # GameState = {"cmd": 1, "players": []}
-      #clients[c]['color']['R'] = 5
 
 def gameLoop(sock):
-   while True:
-      GameState = {"cmd": 1, "players": []}
-      clients_lock.acquire()
-      print (clients)
-      for c in clients:
-         player = {}
-         clients[c]['color'] = {"R": random.random(), "G": random.random(), "B": random.random()}
-         player['id'] = str(c)
-         player['color'] = clients[c]['color']
-         GameState['players'].append(player)
-      s=json.dumps(GameState)
-      print(s)
-      for c in clients:
-         sock.sendto(bytes(s,'utf8'), (c[0],c[1]))
-      clients_lock.release()
-      time.sleep(1)
+    """
+      Takes care of:
+         1. Assigning a random color to every client
+         2. Send the state to all clients
+    """
+
+    pktID = 0  # just to identify a particular network packet while debugging
+    while True:
+        print("Boop")
+        # create a game state object
+        GameState = {"cmd": 1, "pktID": pktID, "players": []}
+        clients_lock.acquire()
+        # print (clients)
+        for c in clients:
+            # create a player object
+            player = {}
+            # assign a random color
+            #Each cube will get a random color but because of the client code this will only happen once
+            clients[c]['color'] = {
+                "R": random.random(),
+                "G": random.random(),
+                "B": random.random()
+            }
+            # fill the player details
+            player['id'] = str(c)
+            player['color'] = clients[c]['color']
+            player['playerPosition'] = clients[c]['playerPosition']
+            GameState['players'].append(player)
+        s = json.dumps(GameState, separators=(",", ":"))
+        #      print(s)
+        # send the gamestate json to all clients
+        for c in clients:
+            sock.sendto(bytes(s, 'utf8'), (c[0], c[1]))
+        clients_lock.release()
+        if (len(clients) > 0):
+            pktID = pktID + 1
+        #This is for the 30 update packets a second this made the colours go quick
+        #time.sleep(1/3) Slow af
+        #time.sleep(1/10) Aight
+        time.sleep(1/30) #Rave time 
+
+
+########################
+""" ENTRY """
+########################
+
 
 def main():
-   port = 12345
-   s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-   s.bind(('', port))
-   start_new_thread(gameLoop, (s,))
-   start_new_thread(connectionLoop, (s,))
-   start_new_thread(cleanClients,(s,))
-   while True:
-      time.sleep(1)
+    print("Running server")
+    """
+      Start 3 new threads 
+    """
+    port = 12345
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind(('', port))
+    start_new_thread(gameLoop, (s, ))
+    start_new_thread(connectionLoop, (s, ))
+    start_new_thread(cleanClients, (s, ))
+    # keep the main thread alive so the children threads stay alive
+    while True:
+        time.sleep(1)
+
 
 if __name__ == '__main__':
-   main()
+    main()
